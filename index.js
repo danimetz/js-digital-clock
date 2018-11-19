11c18f0d3efe9b7c51609cb715ccede3edc0af3c
Danielle Metzner <danimetzner@gmail.com>
2018-11-18 18:00:33 -0800

    Was able to get the clock to update to real time without adding an extra things for the minutes and seconds, using the toLocaleTimeString

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+$(document).ready(function () {
+  window.setInterval(getTime, 1000);
+  function getTime() {
+    // let date = new Date();
+    // let hours = date.getHours();
+    // let minutes = date.getMinutes();
+    // let seconds = date.getSeconds();
+    //
+    // let time = $('<p>' + hours + ':' + minutes + ':' + seconds + '</p>');
+    // $('#clock').html(time)
+    $('#clock').html(new Date(Date.now()).toLocaleTimeString('en-US'));
+  }
+
+});
